293339b8aa5a060074b8f23ccd638c990100698c
Liza Star <lizadstar18@gmail.com>
2020-06-06 12:27:19 -0500

    added initial components

---
+++ src/components/shared/MyNavbar/MyNavbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './MyNavbar.scss';
+
+class MyNavbar extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1> My Navbar </h1>
+      </div>
+    );
+  }
+}
+export default MyNavbar;
