ccd414b7442a27f1dbc866ed1c1500ecc34b896a
Liza Star <lizadstar18@gmail.com>
2020-06-08 19:27:01 -0500

    read works:

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -29,7 +29,7 @@ class Home extends React.Component {
     return (
       <div className="Home">
         <h1> My Stuff </h1>
-        <div className="d=flex flex-wrap">
+        <div className="d-flex flex-wrap">
         { buildStuffCards }
         </div>
       </div>
--- src/components/shared/StuffCard/StuffCard.js
+++ src/components/shared/StuffCard/StuffCard.js
@@ -8,16 +8,16 @@ class StuffCard extends React.Component {
   // }
 
   render() {
-    const { stuff } = this.props;
-    const singleLink = `/stuff/${stuff.id}`;
-    const editLink = `/edit/${stuff.id}`;
+    const { item } = this.props;
+    const singleLink = `/stuff/${item.id}`;
+    const editLink = `/edit/${item.id}`;
     return (
       <div className="StuffCard col-3">
         <div className="card">
           <div className="card-body">
-            <h5 className="card-title">{stuff.itemName}</h5>
-            <img className="card-img-top" src={stuff.itemImage} alt="item" />
-            <p className="card-text">{stuff.itemDescription}</p>
+            <h5 className="card-title">{item.itemName}</h5>
+            <img className="card-img-top" src={item.itemImage} alt="item" />
+            <p className="card-text">{item.itemDescription}</p>
             <Link className="btn btn-info ml-1" to={singleLink}> <i class="far fa-eye"></i> </Link>
             <Link className="btn btn-warning mr-1" to={editLink}> <i className="fas fa-pencil-alt"></i> </Link>
           </div>
